c85a52af8a831f095bd2c0c0940f7f65415fe530
ps0m <ps0m@tut.by>
2022-09-03 12:52:20 +0300

    feat: add functional authorization

--- rslang/src/pages/AuthPage.tsx
+++ rslang/src/pages/AuthPage.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import '../styles/App.scss'
-import Auth from '../components/UI/Auth/Auth'
+import { useState } from 'react';
+import Auth from '../components/UI/Auth/Auth';
+import Header from '../components/UI/Header/Header';
+import PopUpMenu from '../components/UI/PopUpMenu/PopUpMenu';
 
 const AuthPage = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
     <div>
+      {menuActive && <PopUpMenu setActive={setMenuActive} />}
+      <Header setActive={setMenuActive} />
       <Auth />
     </div>
   )
