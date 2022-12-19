b0e6824e61729e6829de63f3b8dc195ee04ac4fd
ps0m <ps0m@tut.by>
2022-12-20 02:09:21 +0300

    fix: update url server

--- rslang/src/constants/constants.ts
+++ rslang/src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const URL_BASE = 'https://rs-rslang.herokuapp.com'
+export const URL_BASE = 'https://react-rslang.onrender.com'
 export const URL_WORDS = `${URL_BASE}/words`
 export const URL_USERS = `${URL_BASE}/users`
 export const URL_SINGIN = `${URL_BASE}/signin`
